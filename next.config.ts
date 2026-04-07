f38d78059dc600d6b8cffc2290e430dea68e9629
Kouhei <test@example.com>
2026-04-08 06:50:25 +0900

    ads.txtのリライトを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/ads.txt",
+        destination: "/api/ads-txt",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
